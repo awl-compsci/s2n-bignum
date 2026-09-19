e6cff1ecccded32999475300404cdc2118542c8f
Angela W. Li <awl.compsci@gmail.com>
2026-09-19 20:53:02 +0000

    Add correctness proof for AVX-512 8x Keccak-f1600
    Eight independent Keccak-f1600 permutations in parallel, one per 64-bit
    lane of the ZMM register file, plus the SysV/Windows asm and Makefile
    wiring. Verified in HOL Light (x86/proofs/sha3_keccak8_f1600_avx512.ml):
    the round-body lemma ROUND_CORRECT and the top-level
    SHA3_KECCAK8_F1600_AVX512_CORRECT (24-round loop over eight states).
    
    Builds on the AVX-512 instruction models added earlier for 4x, which now
    cover the 512-bit (ZMM) dispatch arms for VPBROADCASTQ / VPUNPCKLQDQ /
    VPUNPCKHQDQ that the 8x transpose and round-constant broadcast require.

--- include/s2n-bignum.h
+++ include/s2n-bignum.h
@@ -1310,6 +1310,10 @@ extern void sha3_keccak4_f1600_alt(uint64_t a[S2N_BIGNUM_STATIC 100],const uint6
 extern void sha3_keccak4_f1600_alt2(uint64_t a[S2N_BIGNUM_STATIC 100],const uint64_t rc[S2N_BIGNUM_STATIC 24]);
 #ifdef __x86_64__
 extern void sha3_keccak4_f1600_avx512vl(uint64_t a[S2N_BIGNUM_STATIC 100],const uint64_t rc[S2N_BIGNUM_STATIC 24]);
+
+// Batched 8-way Keccak-f1600 permutation for SHA3
+// Inputs a[200], rc[24]; output a[200]
+extern void sha3_keccak8_f1600_avx512(uint64_t a[S2N_BIGNUM_STATIC 200],const uint64_t rc[S2N_BIGNUM_STATIC 24]);
 #endif
 
 // Point addition on CC curve SM2 in Montgomery-Jacobian coordinates
--- x86/proofs/subroutine_signatures.ml
+++ x86/proofs/subroutine_signatures.ml
@@ -5920,6 +5920,40 @@ let subroutine_signatures = [
    ])
 );
 
+("sha3_keccak4_f1600_avx512vl",
+  ([(*args*)
+     ("a", "uint64_t[100]", (*is const?*)"false");
+     ("rc", "uint64_t[24]", (*is const?*)"true");
+   ],
+   "void",
+   [(* input buffers *)
+    ("a", "100"(* num elems *), 8(* elem bytesize *));
+    ("rc", "24"(* num elems *), 8(* elem bytesize *));
+   ],
+   [(* output buffers *)
+    ("a", "100"(* num elems *), 8(* elem bytesize *));
+   ],
+   [(* temporary buffers *)
+   ])
+);
+
+("sha3_keccak8_f1600_avx512",
+  ([(*args*)
+     ("a", "uint64_t[200]", (*is const?*)"false");
+     ("rc", "uint64_t[24]", (*is const?*)"true");
+   ],
+   "void",
+   [(* input buffers *)
+    ("a", "200"(* num elems *), 8(* elem bytesize *));
+    ("rc", "24"(* num elems *), 8(* elem bytesize *));
+   ],
+   [(* output buffers *)
+    ("a", "200"(* num elems *), 8(* elem bytesize *));
+   ],
+   [(* temporary buffers *)
+   ])
+);
+
 ("sha3_keccak_f1600",
   ([(*args*)
      ("a", "uint64_t[25]", (*is const?*)"false");
